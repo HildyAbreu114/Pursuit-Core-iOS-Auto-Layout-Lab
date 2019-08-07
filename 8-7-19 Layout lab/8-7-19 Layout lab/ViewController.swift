dee25157b64d87e1d9218d0ea4a8835ef4be251f
hildyabreu114 <hildyabreu@pursuit.org>
2019-08-07 14:48:16 -0400

    question 1

---
+++ 8-7-19 Layout lab/8-7-19 Layout lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8-7-19 Layout lab
+//
+//  Created by hildy abreu on 8/7/19.
+//  Copyright © 2019 hildy abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
